21861a680212e7889a28d15dd9839f703e547e66
HebaSalem <puupiiiii@gmail.com>
2022-03-30 22:19:29 +0200

    edit register and login pages

--- src/component/btn/Login.jsx
+++ src/component/btn/Login.jsx
@@ -28,17 +28,17 @@ const Login = () => {
                 </button>
             <form>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <label htmlFor="loginInputEmail" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="loginInputEmail" aria-describedby="emailHelp"/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label htmlFor="loginInputPassword" className="form-label">Password</label>
+                <input type="password" className="form-control" id="loginInputPassword"/>
               </div>
               <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <input type="checkbox" className="form-check-input" id="loginCheck"/>
+                <label className="form-check-label" htmlFor="loginCheck">Check me out</label>
               </div>
               <button type="submit" className="btn btn-dark w-100 mb-5">Submit</button>
             </form>
--- src/component/btn/Register.jsx
+++ src/component/btn/Register.jsx
@@ -28,19 +28,27 @@ const Register = () => {
                 </button>
             <form>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <label htmlFor="registerInput" className="form-label">User Name</label>
+                <input type="email" className="form-control" id="registerInput" required/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="registerInputEmail" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="registerInputEmail" aria-describedby="emailHelp" required/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label htmlFor="registerInputPassword" className="form-label">Password</label>
+                <input type="password" className="form-control" id="registerInputPassword" required minLength='8'/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="registerInputPassword" className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" id="registerInputPassword" required minLength='8'/>
               </div>
               <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <input type="checkbox" className="form-check-input" id="registerCheck"/>
+                <label className="form-check-label" htmlFor="registerCheck">Check me out</label>
               </div>
-              <button type="submit" className="btn btn-dark w-100 mb-5">Submit</button>
+              <button type="submit" className="btn btn-dark w-100 mb-5">Register</button>
             </form>
             </div>
           </div>
