ef295e8bcc8221a05e4fe0eddd4ef3310b502717
HebaSalem <puupiiiii@gmail.com>
2022-03-30 15:31:52 +0200

    remove state componentMounted from products file

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,20 +6,15 @@ const Products = () => {
   const [data ,  setData] = useState([])
   const [filter ,  setFilter] = useState(data)
   const [loading , setLoading] = useState(false)
-  const [componentMounted , setComponentMounted] = useState(true)
   
 
   useEffect(()=>{
     const getProducts = async () => {
       setLoading(true)
       const response = await fetch('https://fakestoreapi.com/products')
-      if(componentMounted) {
         setData(await response.clone().json())
         setFilter(await response.json())
         setLoading(false)
-      }
-      return setComponentMounted(false)
-      
     }
     getProducts()
   } , [])
